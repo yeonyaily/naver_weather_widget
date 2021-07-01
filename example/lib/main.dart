56c87dade111b66a0042131379f1b485c14a583f
yeonyaily <yeonye330@gmail.com>
2021-07-01 16:10:01 +0900

    YJ/0701/main setting

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,55 +8,55 @@ import 'package:flutter/services.dart';
 import 'package:home_widget/home_widget.dart';
 import 'package:workmanager/workmanager.dart';
 
-/// Used for Background Updates using Workmanager Plugin
-void callbackDispatcher() {
-  Workmanager.executeTask((taskName, inputData) {
-    final now = DateTime.now();
-    return Future.wait<bool>([
-      HomeWidget.saveWidgetData(
-        'title',
-        'Updated from Background',
-      ),
-      HomeWidget.saveWidgetData(
-        'message',
-        '${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}',
-      ),
-      HomeWidget.updateWidget(
-        name: 'HomeWidgetExampleProvider',
-        iOSName: 'HomeWidgetExample',
-      ),
-    ]).then((value) {
-      return !value.contains(false);
-    });
-  });
-}
+// /// Used for Background Updates using Workmanager Plugin
+// void callbackDispatcher() {
+//   Workmanager.executeTask((taskName, inputData) {
+//     final now = DateTime.now();
+//     return Future.wait<bool>([
+//       HomeWidget.saveWidgetData(
+//         'title',
+//         'Updated from Background',
+//       ),
+//       HomeWidget.saveWidgetData(
+//         'message',
+//         '${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}',
+//       ),
+//       HomeWidget.updateWidget(
+//         name: 'HomeWidgetExampleProvider',
+//         iOSName: 'HomeWidgetExample',
+//       ),
+//     ]).then((value) {
+//       return !value.contains(false);
+//     });
+//   });
+// }
 
 /// Called when Doing Background Work initiated from Widget
-void backgroundCallback(Uri data) async {
-  print(data);
-
-  if (data.host == 'titleclicked') {
-    final greetings = [
-      'Hello',
-      'Hallo',
-      'Bonjour',
-      'Hola',
-      'Ciao',
-      '哈洛',
-      '안녕하세요',
-      'xin chào'
-    ];
-    final selectedGreeting = greetings[Random().nextInt(greetings.length)];
-
-    await HomeWidget.saveWidgetData<String>('title', selectedGreeting);
-    await HomeWidget.updateWidget(
-        name: 'HomeWidgetExampleProvider', iOSName: 'HomeWidgetExample');
-  }
-}
+// void backgroundCallback(Uri data) async {
+//   print(data);
+//
+//   if (data.host == 'titleclicked') {
+//     final greetings = [
+//       'Hello',
+//       'Hallo',
+//       'Bonjour',
+//       'Hola',
+//       'Ciao',
+//       '哈洛',
+//       '안녕하세요',
+//       'xin chào'
+//     ];
+//     final selectedGreeting = greetings[Random().nextInt(greetings.length)];
+//
+//     await HomeWidget.saveWidgetData<String>('title', selectedGreeting);
+//     await HomeWidget.updateWidget(
+//         name: 'HomeWidgetExampleProvider', iOSName: 'HomeWidgetExample');
+//   }
+// }
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  Workmanager.initialize(callbackDispatcher, isInDebugMode: kDebugMode);
+  // Workmanager.initialize(callbackDispatcher, isInDebugMode: kDebugMode);
   runApp(MaterialApp(home: MyApp()));
 }
 
@@ -66,15 +66,16 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _messageController = TextEditingController();
+  String temperature = '32';
+  String description = '구름많음';
+  String rainFall = '30';
+  String location = '중구 을지로1가';
 
   @override
   void initState() {
     super.initState();
-    // HomeWidget.setAppGroupId('YOUR_GROUP_ID');
     HomeWidget.setAppGroupId('group.com.swfact.home');
-    HomeWidget.registerBackgroundCallback(backgroundCallback);
+    // HomeWidget.registerBackgroundCallback(backgroundCallback);
   }
 
   @override
@@ -84,18 +85,20 @@ class _MyAppState extends State<MyApp> {
     HomeWidget.widgetClicked.listen(_launchedFromWidget);
   }
 
-  @override
-  void dispose() {
-    _titleController.dispose();
-    _messageController.dispose();
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   _titleController.dispose();
+  //   _messageController.dispose();
+  //   super.dispose();
+  // }
 
   Future<void> _sendData() async {
     try {
       return Future.wait([
-        HomeWidget.saveWidgetData<String>('title', _titleController.text),
-        HomeWidget.saveWidgetData<String>('message', _messageController.text),
+        HomeWidget.saveWidgetData<String>('temperature', temperature),
+        HomeWidget.saveWidgetData<String>('description', description),
+        HomeWidget.saveWidgetData<String>('rainFall', rainFall),
+        HomeWidget.saveWidgetData<String>('location', location),
       ]);
     } on PlatformException catch (exception) {
       debugPrint('Error Sending Data. $exception');
@@ -114,11 +117,15 @@ class _MyAppState extends State<MyApp> {
   Future<void> _loadData() async {
     try {
       return Future.wait([
-        HomeWidget.getWidgetData<String>('title', defaultValue: 'Default Title')
-            .then((value) => _titleController.text = value),
-        HomeWidget.getWidgetData<String>('message',
-                defaultValue: 'Default Message')
-            .then((value) => _messageController.text = value),
+        HomeWidget.getWidgetData<String>('temperature', defaultValue: 'Default temperature')
+            .then((value) => temperature = value),
+        HomeWidget.getWidgetData<String>('description', defaultValue: 'Default description')
+            .then((value) => description = value),
+        HomeWidget.getWidgetData<String>('rainFall', defaultValue: 'Default rainFall')
+            .then((value) => rainFall = value),
+        HomeWidget.getWidgetData<String>('location',
+                defaultValue: 'Default location')
+            .then((value) => location = value),
       ]);
     } on PlatformException catch (exception) {
       debugPrint('Error Getting Data. $exception');
@@ -145,14 +152,14 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
-  void _startBackgroundUpdate() {
-    Workmanager.registerPeriodicTask('1', 'widgetBackgroundUpdate',
-        frequency: Duration(minutes: 15));
-  }
-
-  void _stopBackgroundUpdate() {
-    Workmanager.cancelByUniqueName('1');
-  }
+  // void _startBackgroundUpdate() {
+  //   Workmanager.registerPeriodicTask('1', 'widgetBackgroundUpdate',
+  //       frequency: Duration(minutes: 15));
+  // }
+  //
+  // void _stopBackgroundUpdate() {
+  //   Workmanager.cancelByUniqueName('1');
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -163,18 +170,10 @@ class _MyAppState extends State<MyApp> {
       body: Center(
         child: Column(
           children: [
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Title',
-              ),
-              controller: _titleController,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Body',
-              ),
-              controller: _messageController,
-            ),
+            Text(temperature),
+            Text(description),
+            Text(rainFall),
+            Text(location),
             ElevatedButton(
               onPressed: _sendAndUpdate,
               child: Text('Send Data to Widget'),
@@ -187,16 +186,16 @@ class _MyAppState extends State<MyApp> {
               onPressed: _checkForWidgetLaunch,
               child: Text('Check For Widget Launch'),
             ),
-            if (Platform.isAndroid)
-              ElevatedButton(
-                onPressed: _startBackgroundUpdate,
-                child: Text('Update in background'),
-              ),
-            if (Platform.isAndroid)
-              ElevatedButton(
-                onPressed: _stopBackgroundUpdate,
-                child: Text('Stop updating in background'),
-              )
+            // if (Platform.isAndroid)
+            //   ElevatedButton(
+            //     onPressed: _startBackgroundUpdate,
+            //     child: Text('Update in background'),
+            //   ),
+            // if (Platform.isAndroid)
+            //   ElevatedButton(
+            //     onPressed: _stopBackgroundUpdate,
+            //     child: Text('Stop updating in background'),
+            //   )
           ],
         ),
       ),
